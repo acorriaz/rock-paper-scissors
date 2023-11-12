ac78f9d8f78d56b15fca2562a6c94f8608e63c33
acorriaz <mikey.swx@gmail.com>
2023-11-12 12:30:13 +0700

    Add pusedocode for rock paper scissors game

---
+++ script.js
@@ -0,0 +1,24 @@
+// Pseudocode
+// This Programs will show winner in 5 round rock paper scissors game (in console)
+
+// let roundCounter
+// let winCounter
+
+// get input from player
+// get input from computer with random math and change number to answer
+
+// loop for 5 times
+// function to determined who the winner is
+
+// update playerInput
+// update computerInput
+
+// roundCounter++
+
+// if draw
+//   show what you draw
+//     else if win
+//       counter++
+//       show what you win
+//         else
+//           show what you lose
