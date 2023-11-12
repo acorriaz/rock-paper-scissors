dd52b70e5d6aa7ad3cc9adebf96b41ece7833117
acorriaz <mikey.swx@gmail.com>
2023-11-12 13:49:14 +0700

    Update pseudocode and Add player input and computer input

--- script.js
+++ script.js
@@ -2,23 +2,55 @@
 // This Programs will show winner in 5 round rock paper scissors game (in console)
 
 // let roundCounter
-// let winCounter
+// let playerCounter
+// let computerCounter
 
 // get input from player
 // get input from computer with random math and change number to answer
 
-// loop for 5 times
-// function to determined who the winner is
+// function to playRound of rock paper scissors
 
-// update playerInput
-// update computerInput
+//   update playerInput
+//   update computerInput
 
-// roundCounter++
+//   roundCounter++
 
-// if draw
-//   show what you draw
-//     else if win
-//       counter++
-//       show what you win
-//         else
-//           show what you lose
+//   if draw
+//     show what you draw
+//       else if win
+//         counter++
+//         show what you win
+//           else
+//             show what you lose
+
+// make function to playRound 5 times
+//   loop function playRound 5 times
+//   if 
+//     playerCounter > computerCounter player win
+//     else if playerCounter = computerCounter draw
+//     else computer win
+//   reset counter
+
+let roundCounter = 0;
+let playerCounter = 0;
+let computerCounter = 0;
+
+const getPlayerSelection = (n) => {
+  n = prompt("What will you choose Rock, Paper, Scissors").toLowerCase();
+  return n;
+};
+
+const getComputerSelection = (choice) => {
+  choice = Math.floor(Math.random() * 3 + 1);
+  switch (choice) {
+    case 1:
+      choice = "rock";
+      return choice;
+    case 2:
+      choice = "paper";
+      return choice;
+    case 3:
+      choice = "scissors";
+      return choice;
+  }
+};
